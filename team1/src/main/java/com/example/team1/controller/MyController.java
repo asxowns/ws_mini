e3d61c5e4059129beb28aafcb74ac6fce7eeaed3
Kim TaeJun <1000pgm@gmail.com>
2024-05-02 15:52:44 +0900

    컨트롤러 중간단계 8

--- team1/src/main/java/com/example/team1/controller/MyController.java
+++ team1/src/main/java/com/example/team1/controller/MyController.java
@@ -22,144 +22,147 @@ import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class MyController {
-	
+
 	@Autowired
 	private IBbsDao dao;
-	
+
+	@Autowired
+	HttpServletRequest request;
+
+	@Autowired
+	HttpServletResponse response;
 	// 인덱스
 	@RequestMapping("/index")
 	public String root() {
 		return "index";
 	}
-	
+
 	// 로그인 폼
 	@GetMapping("/loginForm")
 	public String loginForm() {
-		
+
 		return "loginForm";
 	}
-	
+
 	// 로그인하기
 	@PostMapping("/login")
-	public String login(HttpServletRequest request, HttpServletResponse response, @RequestParam("id") String id, @RequestParam("pw") String pw) throws IOException {
-		
+	public String login(@RequestParam("id") String id, @RequestParam("pw") String pw) throws IOException {
+
 		response.setContentType("text/html; charset=utf-8");
 		request.setCharacterEncoding("UTF-8");
-		
+
 		PrintWriter out = response.getWriter();
 		Member result = dao.login(id, pw);
-		
-		if(result != null) {
+
+		if (result != null) {
 			HttpSession session = request.getSession();
 			session.setAttribute("id", id);
 			session.setAttribute("pw", pw);
+			session.setAttribute("name", result.getName());
 			out.print(String.format("<script>alert('로그인성공');</script>"));
 			return "success";
-		}else {
+		} else {
 			out.print(String.format("<script>alert('로그인실패 다시입력해주세요.');</script>"));
 			return "loginForm";
 		}
 	}
-	
+
+	@GetMapping("/logout")
+	public String logout() {
+		HttpSession session = request.getSession();
+		
+		session.invalidate();
+
+		return "index";
+	}
+
 	@GetMapping("/myPage")
 	public String myPage() {
 		return "myPage";
 	}
-	
+
 	// 글작성 폼
 	@GetMapping("/writeForm")
 	public String writeForm() {
-		
+
 		return "writeForm";
 	}
-	
+
 	// 글작성하기
 	@PostMapping("/write")
-	public String write(Bbs bbs, HttpServletResponse response, HttpServletRequest request) throws Exception{
+	public String write(Bbs bbs) throws Exception{
 		System.out.println(bbs.getId());
 		HttpSession session = request.getSession();
-		bbs.setId((String)session.getAttribute("id"));
+		bbs.setId((String) session.getAttribute("id"));
 		response.setContentType("text/html; charset=utf-8");
 		request.setCharacterEncoding("UTF-8");
-		
+
 		PrintWriter out = response.getWriter();
 		dao.write(bbs);
-		
-		if(bbs.getId().equals("")) {
+
+		if (bbs.getId().equals("")) {
 			out.print(String.format("<script>alert('아이디를 입력해주세요.');</script>"));
 			return "writeForm";
-		}else if(bbs.getTitle().equals("")) {
+		} else if (bbs.getTitle().equals("")) {
 			out.print(String.format("<script>alert('제목을 입력해주세요.');</script>"));
 			return "writeForm";
-		}else if(bbs.getContent().equals("")) {
+		} else if (bbs.getContent().equals("")) {
 			out.print(String.format("<script>alert('내용을 입력해주세요.');</script>"));
 			return "writeForm";
-		}else if(bbs.getTarget().equals("")) {
+		} else if (bbs.getTarget().equals("")) {
 			out.print(String.format("<script>alert('받는 사람을 입력해주세요.');</script>"));
 			return "writeForm";
-		}else {
+		} else {
 			return "redirect:list";
 		}
-			
-		
+
 	}
-	
-	//전체 리스트
+
+	// 전체 리스트
 	@GetMapping("/list")
 	public String list(Model model) {
-		
+
 		model.addAttribute("list", dao.list());
-		
+
 		return "list";
 	}
-	
-	//보낸 리스트
+
+	// 보낸 리스트
 	@GetMapping("/sendList")
 	public String sendList(@RequestParam("id") String id, Model model) {
-		
+
 		model.addAttribute("list", dao.sendList(id));
-		
+
 		return "listSend";
 	}
-	
-	//받은 리스트
+
+	// 받은 리스트
 	@GetMapping("/receiveList")
 	public String receiveList(@RequestParam("id") String id, Model model) {
-		
+
 		model.addAttribute("list", dao.receiveList(id));
-		
+
 		return "listReceive";
 	}
-	
+
 	// 디테일 페이지
 	@GetMapping("/detail")
 	public String detail(@RequestParam("bno") int bno, Model model) {
-		
+
 		Bbs bbs = dao.detail(bno);
-		
+
 		model.addAttribute("dto", bbs);
-		
+
 		return "detail";
 	}
-	
+
 	// 삭제
 	@GetMapping("/delete")
 	public String delete(@RequestParam("bno") int bno) {
-		
+
 		dao.delete(bno);
-		
+
 		return "redirect:list";
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
 
 }
